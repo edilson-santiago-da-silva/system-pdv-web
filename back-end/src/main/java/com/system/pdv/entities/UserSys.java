d2018d422bc248d7caaebf8eff6db998bba24d22
Edilson Santiago <edilsonsantiagoe7@gmail.com>
2024-07-01 09:18:48 -0300

    spring security JWT part 1

--- back-end/src/main/java/com/system/pdv/entities/UserSys.java
+++ back-end/src/main/java/com/system/pdv/entities/UserSys.java
@@ -1,17 +1,23 @@
 package com.system.pdv.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
+import com.system.pdv.user.UserRole;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name ="user_sys")
-public class UserSys implements Serializable {
+public class UserSys implements Serializable, UserDetails {
 	private static final long serialVersionUID = 1L;
 	
 	@Id 
@@ -19,6 +25,7 @@ public class UserSys implements Serializable {
 	private Integer id;
 	private String email;
 	private String password_sys;
+	private UserRole role;
 	
 	public UserSys() {
 	}
@@ -44,13 +51,52 @@ public class UserSys implements Serializable {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public String getPassword() {
 		return password_sys;
 	}
 
 	public void setPassword(String password_sys) {
 		this.password_sys = password_sys;
+	}
+
+	public UserRole getRole() {
+		return role;
+	}
+
+	public void setRole(UserRole role) {
+		this.role = role;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		if (this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+		else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return UserDetails.super.isAccountNonExpired();
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 
 	@Override
