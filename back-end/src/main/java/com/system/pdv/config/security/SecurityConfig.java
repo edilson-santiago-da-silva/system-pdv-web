ade3697c36028177fb71d2a74c9dc203e278f45d
Edilson Santiago <edilsonsantiagoe7@gmail.com>
2024-07-06 10:17:07 -0300

    spring security finalization

--- back-end/src/main/java/com/system/pdv/config/security/SecurityConfig.java
+++ back-end/src/main/java/com/system/pdv/config/security/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers("/swagger-ui//index.html#", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll() // apenas para teste, ajustar depois, min.47:45
                         .requestMatchers(HttpMethod.DELETE, "/users").hasRole("ADMIN")
                         .anyRequest().authenticated()
